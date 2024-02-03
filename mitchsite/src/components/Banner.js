43028f908dabf658a6b800c85533d9599e587fd6
Mitch <firestate15@gmail.com>
2024-02-02 18:26:45 -0800

    Rough Draft working, more updates required

--- mitchsite/src/components/Banner.js
+++ mitchsite/src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Software Engineer", "Critical Thinker", "Problem Solver" ];
   const period = 2000;
 
   useEffect(() => {
@@ -56,8 +56,8 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Mitchell Mercer`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Engineer", "Blank", "Blank" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h1>{`Mitchell Mercer:`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Engineer", "Blank", "Blank" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Highly motivated California State Universtiy Alumnus excited to make an impact in Tech</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
--- mitchsite/src/components/NavBar.js
+++ mitchsite/src/components/NavBar.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 import {useState, useEffect} from "react";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon2 from '../assets/img/github-mark-white.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
 
@@ -52,9 +52,9 @@ export const NavBar = () => {
           </Nav>
           <span className='navbar-text'>
             <div className='social-icon'>
-              <a href='#'><img src = {navIcon1} alt = "" /> </a>
-              <a href='#'><img src = {navIcon2} alt = "" /> </a>
-              <a href='#'><img src = {navIcon3} alt = "" /> </a>
+              <a href='https://www.linkedin.com/in/mitchell-mercer-972a5220b/'><img src = {navIcon1} alt = "" /> </a>
+              <a href='https://github.com/matfitchell'><img src = {navIcon2} alt = "" /> </a>
+              
             </div>
             <button className='vvd' onClick={ () => console.log('connect')}><span>Let's Connect</span></button>
           </span>
