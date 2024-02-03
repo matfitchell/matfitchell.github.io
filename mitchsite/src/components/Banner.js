e94a7255efa6315309d96ceda3f75a08c3e9f642
Mitch <firestate15@gmail.com>
2024-02-02 17:17:58 -0800

    This might break stuff

--- mitchsite/src/components/Banner.js
+++ mitchsite/src/components/Banner.js
@@ -1,64 +1,77 @@
-import { useEffect, useState } from "react";
-import { Container, Row } from "react-bootstrap";
-import { Col} from "react";
-import { ArrowRightCircle } from "react-bootstrap-icons";
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
-import headderImg from "../assets/img/headder-Img.svg";
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const period = 2000;
 
-export const Banner = () =>{
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
 
-    const toRotate = ["Software Engineer", "blank" , "holder"];
-    const [loopnum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const [text, setText] = useState('');
-    const period = 2000;
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    return () => { clearInterval(ticker) };
+  }, [text])
 
-    useEffect(()=> {
-        let ticker = setInterval(() =>{
-            tick();
-        }, delta)
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-        return () => {clearInterval(ticker)};
-    },[text])
+    setText(updatedText);
 
-    const tick = () => {
-        let i  = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length -1 ) : fullText.substring(0, text.length +1); 
-
-        setText(updatedText);
-
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta /2)
-        }
-
-        if (!isDeleting && updatedText === fullText){
-            setIsDeleting(true);
-            setDelta(period);
-        }
-        else if (isDeleting && updatedText === ""){
-            setIsDeleting(false);
-            setLoopNum(loopnum +1);
-            setDelta(500);
-        }
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
     }
 
-    return (
-        <section className="banner" id="home">
-            <Container>
-                <Row className="banner" id="home">
-                    <Col xs={12} md={6} xl={7}> 
-                        <span className="tagLine">Welcome to my portfolio</span>
-                        <h1>{"Hi I'm Mitchell Mercer"}<span className="wrap">software engineer</span></h1>
-                        <p>New California State University Alumnus Looking to venture into the world of complex probelm solving and creation of satisfying solutions</p>
-                        <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25} /></button>
-                    </Col>
-                    <Col xs={12} md={6} xl={5}> 
-                        <img src = {headderImg} alt ="Headder Img" />
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    )
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Mitchell Mercer`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Software Engineer", "Blank", "Blank" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
 }
